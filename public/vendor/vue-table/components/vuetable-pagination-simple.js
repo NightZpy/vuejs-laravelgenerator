e4b823d3b13af5460015fe9992abae41f196107f
Lenyn Alcántara <nightzpy@gmail.com>
2016-05-31 08:08:00 +0000

    Deleting some files unused

--- public/vendor/vue-table/components/vuetable-pagination-simple.js
+++
@@ -1,9 +0,0 @@
-Vue.component('vuetable-pagination-simple', {
-    template: '#vuetable-pagination-simple-template',
-    mixins: [paginationMixin],
-    methods: {
-        loadPage: function(page) {
-            this.$dispatch('vuetable-pagination:change-page', page)
-        },
-    },
-});  
